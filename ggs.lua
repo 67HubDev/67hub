e376e76933f88abb563d35631b907207ca7e137f
hizuma_67 <bbuichihieu@gmail.com>
2026-03-08 00:13:58 +0700

    Update ggs.lua

--- ggs.lua
+++ ggs.lua
@@ -1,3 +1,3 @@
-[Phiên bản 4.2]
-- Nếu bị lỗi hãy vào Discord để hỗ trợ,đợi scprit load!
+[Phiên bản 4.3]
+- Nếu bị lỗi hãy vào Discord để hỗ trợ,hãy đợi scprit load!
 - Sẽ thêm script mới,xem update ở tt
